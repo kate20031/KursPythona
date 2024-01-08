76ce4dec9fad2c79ff642d233cd0bade11284bec
kate20031 <83357098+kate20031@users.noreply.github.com>
2024-01-08 02:29:32 +0100

    Update Ulamki_3.py

--- Lab9/Ulamki_3.py
+++ Lab9/Ulamki_3.py
@@ -1,46 +1,41 @@
-import unittest
-import pytest
-import self
-from Ulamki_3 import Ulamek
-from unittest.mock import MagicMock
+import math
 
+class Ulamek:
+    def __init__(self, licznik, mianownik):
+        assert mianownik != 0
+        self.licznik = licznik
+        self.mianownik = mianownik
+        self.skrocenie()
 
+    # Wersja2 __slots__ = ('licznik', 'mianownik')
 
-def test_zwykły_zapisu():
-        u1 = Ulamek(1, -6)
-        Ulamek.zapisz_do_pliku(u1, "plik_1")
-        ul2 = Ulamek.odczytaj_z_pliku(self, "plik_1")
-        print(ul2)
+    def skrocenie(self):
+        nwd = math.gcd(self.licznik, self.mianownik)
+        self.licznik //= nwd
+        self.mianownik //= nwd
+        if (self.licznik < 0 and self.mianownik < 0):
+            self.licznik = abs(self.licznik)
+            self.mianownik = abs(self.mianownik)
 
-@pytest.mark.parametrize("u1, u2, oczekiwana_suma", [(Ulamek(1, 3), Ulamek(1, 2), Ulamek(5, 6)),
-                                                     (Ulamek(-1, 3), Ulamek(-2, 3), Ulamek(-1, 1)) ])
-def test_suma_parametr(u1, u2, oczekiwana_suma):
-    suma = u1 + u2
-    assert suma.licznik == oczekiwana_suma.licznik
-    assert suma.mianownik == oczekiwana_suma.mianownik
+        if (self.licznik > 0 and self.mianownik < 0):
+            self.licznik *= -1
+            self.mianownik = abs(self.mianownik)
 
-@pytest.fixture
-def zapisz_mocked(monkeypatch):
-    mock_open = MagicMock()
-    monkeypatch.setattr(Ulamek, 'zapisz_do_pliku', mock_open)
-    return mock_open
+    def __str__(self):
+        if self.licznik == 0 or self.mianownik == 1:
+            return f"Ulamek: {self.licznik}"
 
-@pytest.fixture
-def odczytaj_mocked(monkeypatch):
-    ulamek = Ulamek(-1, 7)
-    with monkeypatch.context() as m:
-        m.setattr(Ulamek, 'odczytaj_z_pliku', MagicMock(return_value=ulamek))
-        yield m
+        return f"Ulamek: {self.licznik}/{self.mianownik}"
 
-def test_zapisz_i_odczytaj_mock(zapisz_mocked, odczytaj_mocked):
-    ulamek = Ulamek(1, -7)
-    ulamek.zapisz_do_pliku("plik1")
-    zapisz_mocked.assert_called_once_with("plik1")
+    def __add__(self, y):
+        return Ulamek(self.licznik * y.mianownik + y.licznik * self.mianownik, self.mianownik * y.mianownik)
 
-    odczytany_ulamek = Ulamek.odczytaj_z_pliku("plik1")
+    def zapisz_do_pliku(self, nazwa_pliku):
+        with open(nazwa_pliku, 'w') as plik:
+            plik.write(f"{self.licznik} / {self.mianownik}")
 
-    assert ulamek.licznik == odczytany_ulamek.licznik
-    assert ulamek.mianownik == odczytany_ulamek.mianownik
+    def odczytaj_z_pliku(self, nazwa_pliku):
+        with open(nazwa_pliku, 'r') as plik:
+            ulamek_str = plik.read().strip().split('/')
 
-if __name__ == '__main__':
-    unittest.main()
+        return Ulamek(licznik=int(ulamek_str[0]), mianownik=int(ulamek_str[1]))
